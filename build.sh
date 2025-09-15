6ccdaa53c1549b2a3c6a820e042fb62cea88c1e6
ItsRatcha <mynameisnon23@gmail.com>
2025-09-16 02:15:37 +0700

    force postgres

--- build.sh
+++ build.sh
@@ -10,6 +10,9 @@ pip install -r requirements.txt
 # Navigate to Django project folder
 cd roombooking
 
+# Set environment variables
+export DATABASE_URL=$DATABASE_URL
+
 # Run migrations
 python manage.py migrate --noinput
 
