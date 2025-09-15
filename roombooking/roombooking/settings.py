834b7ee39f9b8dbfa25c55ac1fc69bdd239a2e73
ItsRatcha <mynameisnon23@gmail.com>
2025-09-16 01:08:58 +0700

    update templates

--- roombooking/roombooking/settings.py
+++ roombooking/roombooking/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Static files (WhiteNoise)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # This should match what your build.sh uses
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Recommended for Django 4.2+
 STORAGES = {
@@ -82,10 +82,11 @@ ROOT_URLCONF = 'roombooking.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
